3cda4f68cd30542d2e5835b502619654fc8d476e
Victor Mumo <victormumo@Victors-MacBook-Pro.local>
2023-10-23 21:49:25 +0300

    index.js update

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# IMPORTANT LINK FOR PROJECT
+# "Token Stacking Dapp Project", 
 
-@theblockchaincoders
-
-Build your web3 Dapp "Token Stacking Dapp Project", in which you can provide users to stack their ERC20 token and earn reward, you can also provide multiple token stacking contract.
+You can provide users to stack their ERC20 token and earn reward, you can also provide multiple token stacking contract.
 
 # token_stacking_dapp
---
+++ js/index.js
@@ -0,0 +1,56 @@
+//FUNCTION CALL
+loadInitialData("sevenDays");
+connectMe("metamask_wallet");
+function connectWallet() {}
+
+function openTab(event, name) {
+    console.log(name);
+    contractCall = name;
+    getSelectedTab(name);
+    loadInitialData(name);
+}
+
+async function loadInitialData(sClass) {
+    console.log(sClass);
+    try {
+        clearInterval(countDownGlobal);
+        
+        let cObj = new web3Main.eth.Contract(
+            SELECT_CONTRACT[_NETWORK_ID].STACKING.abi,
+            SELECT_CONTRACT[_NETWORK_ID].STACKING[sClass].address
+        );
+
+        //ID ELEMENT DATA
+        let totalUsers = await cObj.methods.getTotalUsers().call();
+        let cApy = await cObj.methods.getAPY().call();
+        //GET USER
+        let userDetail = await cObj.methods.getUser(currentAddress).call();
+
+        const user = {
+            lastRewardCalculationTime: userDetail.lastRewardCalculationTime,
+            lastStakeTime: userDetail.lastStakeTime,
+            rewardAmount: userDetail.rewardAmount,
+            rewardsClaimedSoFar: userDetail.rewardsClaimedSoFar,
+            stakeAmount: userDetail.stakeAmount,
+            address: currentAddress,
+        };
+        localStorage.setItem("User", JSON.stringify(user));
+
+        let userDetailBal = userDetail.stakeAmount / 10 ** 18;
+        document.getElementById(
+            "total-locked-user-token"
+        ).innerHTML = `${userDetailBal}`;
+
+        //ELEMENTS --ID
+        document.getElementById(
+            "num-of-staackers-value"
+        ).innerHTML = `${totalUsers}`;
+        document.getElementById("apy-value-feature").innerHTML = `${cApy} %`;
+
+        //ClASS ELEMENT DATA
+        let totalLockedTokens = await cObj.methods.getTotalStakedTokens().call();
+        let earlyUnstakedFee = await cObj.methods
+        .getEarlyUnstakeFeePercentage()
+        .call();
+    }
+}
